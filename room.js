3cef9157fa91567396eca6c9d7552e3f744fe62a
martinloman <martin.loman@gmail.com>
2014-01-15 21:00:27 +0100

    Added test button.

--- room.js
+++ room.js
@@ -3,27 +3,31 @@ var rooms = {};
 
 exports.init = function(io){
   io.sockets.on('connection', function (socket) {
-    socket.on('join', function (roomName, user) {
-      socket.join(roomName);
-      socket.set('user', user, function() {
-        var room = rooms[roomName] || { roomName : roomName, users: [] };
+
+    socket.on('join', function (data) {
+      console.log(data.user.id + ' loggar in');
+      socket.join(data.roomName);
+      socket.set('user', data.user, function() {
+        
+        var room = rooms[data.roomName] || { roomName : data.roomName, users: [] };
         
         var existingUser = room.users.filter(function(existingUser) {
-          return existingUser.id === user.id
+          return existingUser.id === data.user.id
         }).shift(); 
         
-        if (!existingUser) room.users.push(user);
+        if (!existingUser) room.users.push(data.user);
         else {
           existingUser.lastJoinDate = new Date(); 
         }
 
-        rooms[roomName] = room;
+        rooms[data.roomName] = room;
         socket.emit(room);
+        console.log(data.user.id + ' loggade in i ' + data.roomName);
       })
     });
 
     socket.on('addSong', function (song) {
-      
+
     });
   });
 }
