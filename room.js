4d8dd2423d3b990dec0a24409a3df44481c8974e
martinloman <martin.loman@gmail.com>
2014-01-15 19:57:20 +0100

    inital commit

---
+++ room.js
@@ -0,0 +1,9 @@
+
+exports.init = function(io){
+	io.sockets.on('connection', function (socket) {
+	  socket.emit('news', { hello: 'world' });
+	  socket.on('my other event', function (data) {
+	    console.log(data);
+	  });
+	});
+}
