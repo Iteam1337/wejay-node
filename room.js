fd48d1e7708a9350f2b6309a8fe1b737d62aa626
martinloman <martin.loman@gmail.com>
2014-01-15 20:27:08 +0100

    Room login added

--- room.js
+++ room.js
@@ -1,9 +1,29 @@
+var rooms = {};
+
 
 exports.init = function(io){
-	io.sockets.on('connection', function (socket) {
-	  socket.emit('news', { hello: 'world' });
-	  socket.on('my other event', function (data) {
-	    console.log(data);
-	  });
-	});
+  io.sockets.on('connection', function (socket) {
+    socket.on('join', function (roomName, user) {
+      socket.join(roomName);
+      socket.set('user', user, function() {
+        var room = rooms[roomName] || { roomName : roomName, users: [] };
+        
+        var existingUser = room.users.filter(function(existingUser) {
+          return existingUser.id === user.id
+        }).shift(); 
+        
+        if (!existingUser) room.users.push(user);
+        else {
+          existingUser.lastJoinDate = new Date(); 
+        }
+
+        rooms[roomName] = room;
+        socket.emit(room);
+      })
+    });
+
+    socket.on('addSong', function (song) {
+      
+    });
+  });
 }
